bdaa3709e1bf8983eeec2f241a566520ac36fdcb
gunm <gunm@cisco.com>
2019-02-03 14:53:18 +0530

    Css Module part 1

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 # React-2019
 Change different branch to get explore.
-* CSS Module
+## CSS Module
+* Advantage of css module is  that you no longer have to worry about naming conflicts or collisions in your CSS. All your CSS is now locally scoped to the component you're using it in.
 
+### STEPS
+* run the below script
+
+```
+    npm run eject
+```
+* Add the below line in your webpack.config.js (search exclude: cssModuleRegex,) and replace the below changes
+
+```
+    exclude: cssModuleRegex,
+    use: getStyleLoaders({
+        importLoaders: 1,
+        modules: true,
+        localIdentName: '[name]__[local]__[hash:base64:5]'
+        
+    }),
+```
+* Now you can use css as Module like below
+
+```
+import mycssModule from './App.css';
+
+
+<div className={mycssModule.App}>
+
+
+classes.push( mycssModule.red );
+```
--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import './App.css';
+import mycssModule from './App.css';
 import PersonImport from './Person/Person';
 // For component name use only caps (custom component) , small letter element is default jsx.
 class App extends Component {
@@ -72,14 +72,14 @@ class App extends Component {
 
     const classes = [];
     if ( this.state.persons.length <= 2 ) {
-      classes.push( 'red' ); // classes = ['red']
+      classes.push( mycssModule.red ); // classes = ['red']
     }
     if ( this.state.persons.length <= 1 ) {
-      classes.push( 'bold' ); // classes = ['red', 'bold']
+      classes.push( mycssModule.bold ); // classes = ['red', 'bold']
     }
     
     return (
-        <div className="App">
+        <div className={mycssModule.App}>
           <h1>Hi, I'm a React App</h1>
           <p className={classes.join( ' ' )}>This is really working!</p>
           <button style={styleguide} onClick={this.togglePersonsHandler}>Toggle Persons</button>
