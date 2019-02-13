658567f7f4cdf520a54a84e2c6d358b926b3b050
gunm <gunm@cisco.com>
2019-02-13 19:29:36 +0530

    Adjacent element render

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # React-2019
 Change different branch to get explore.
-## Functional Component React Hook
+## Render JSX Adjacent
+* Till now we have to give render JSX as single root now we can send adjacent JSX as follows,
+* Multiple elements sits next to each other
+* return as comma seperated array with unique Id
 
-###
+```
+    return [
+                <p key="i1" onClick={this.props.deleteme}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+                <p key="i2" >{this.props.children}</p>,
+                <input type="text" key="i3" onChange={this.props.heychanged} value={this.props.name} />
+        ];
+```
+
 
 
 
--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -5,13 +5,11 @@ import mycssClassModule from './Person.css';
 class PersonExport extends Component {
     render() {
         console.log('[Person.js] rendering...');
-        return (
-            <div className={mycssClassModule.Person}>
-                <p onClick={this.props.deleteme}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.heychanged} value={this.props.name} />
-            </div>
-        );
+        return [
+                <p key="i1" onClick={this.props.deleteme}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+                <p key="i2" >{this.props.children}</p>,
+                <input type="text" key="i3" onChange={this.props.heychanged} value={this.props.name} />
+        ];
     }
 }
 
